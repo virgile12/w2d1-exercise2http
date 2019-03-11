7886ad529a3dd1baeb952de11692759f9ba8c0e0
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-11 22:32:31 +0000

    exercise 2 pushing

---
+++ index.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err; 
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('downloading');
+         response.headers['jpeg']
+
+         
+       })
+       .on('end', function (end) {
+           console.log('nice job downloaded')
+       })
+       .pipe(fs.createWriteStream('./future.jpeg'));
